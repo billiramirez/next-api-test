96acc97a6e21061b5ac6c928d656cef726966f5c
Billi Ramirez <ramirezbilly42@outlook.com>
2020-07-22 07:35:32 -0600

    initial commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+  if (req.method === "POST") {
+    res.statusCode = 200;
+    res.json({ name: "John Doe" });
+  }
+  return res.status(404).json({
+    error: {
+      code: "not_found",
+      message: "The requested endpoint does not exist",
+    },
+  });
+};
